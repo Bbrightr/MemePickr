8125a1e8bb8455972b6d5fe0ab86e9f5540962f1
Bright Obiri Gyan <74192474+Bbrightr@users.noreply.github.com>
2025-03-09 23:52:36 +0100

    updated

--- index.js
+++ index.js
@@ -170,18 +170,16 @@ const catsData = [
 ]
 
 function getEmotionsArray(cats){
-//     const emotionsArray = []
-//     for (let i = 0; i < cats.length; i++){
-//         for (let j=0; j < cats[i].emotionTags.length; j++){
-//                 emotionsArray.push(cats[i].emotionTags[j])
-//         }
-//     }
-// console.log(emotionsArray)
 
- for (let cat of cats){
-    console.log(cat)
- }
-}
-
-getEmotionsArray(catsData)
+        const emotionsArray = []
+        
+        for (let cat of cats){
+            for (let emotion of cat.emotionTags){
+                emotionsArray.push(emotion)
+            }
+            
+        }
+        return emotionsArray
+    }
+    
 
