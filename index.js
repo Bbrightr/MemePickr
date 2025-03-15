f188888f40b7bb2bf85f088c7e467154976c0fc8
Bright Obiri Gyan <74192474+Bbrightr@users.noreply.github.com>
2025-03-15 20:51:19 +0100

    cleaing multipies

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            emotionsArray.push(emotion)
+            if (emotionsArray.includes(emotion)){
+            } else {
+                emotionsArray.push(emotion)
+
+            }
         }
     }
     return emotionsArray
